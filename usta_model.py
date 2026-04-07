e518b8ddc236630de14c37a8b4580877534f7b3b
iremdinc22 <iremdinc211@gmail.com>
2026-04-07 13:50:15 +0300

    feat: add generate method to UstaModel for iterative token prediction

--- usta_model.py
+++ usta_model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-from usta_layer_norm import UstaLayerNorm
 from usta_decoder_block import UstaDecoderBlock
 from usta_embedding import UstaEmbedding
 
@@ -14,19 +13,40 @@ class UstaModel(nn.Module):
     self.layers = nn.Sequential(
       *[UstaDecoderBlock(embedding_dim, num_heads, context_length) for _ in range(num_layers)]
     )
+
     self.lm_head = nn.Linear(embedding_dim, vocab_size)
 
+  def forward(self, x: torch.Tensor):
+    x = self.embedding(x) # dictionary meaning of the tokens (words)
+    
+    x = self.layers(x)
+    x = self.lm_head(x)
 
-  def forward(self, x):
-      x = self.embedding(x)  # dictionary meaning of the tokens (words)
-      x = self.layers(x)
-      x = self.lm_head(x)
-      return x
+    return x
+
+
+  """ out = u_model(torch.tensor(new_tokens))
+
+  probs = torch.softmax(out[-1], dim=-1)
+  max_prob, max_index = torch.max(probs, dim=-1)
+  max_prob, max_index, probs
+  """
+
+  def generate(self, x: torch.Tensor, max_new_tokens: int): # top_k, top_p, temperature
+    tokens = x.detach().cpu().numpy().tolist()
+    
+    for _ in range(max_new_tokens):
+      out = self.forward(x)
+      probs = torch.softmax(out[-1], dim=-1)
+      _, max_index = torch.max(probs, dim=-1)
+      tokens.append(max_index.item())
+      if max_index == 59 or len(tokens) > 32: # <eos> and max context length
+        break
+      
+      x = torch.tensor(tokens)
+
+    return tokens
+
+
     
     
-      
-      
-      
-      
-      
-      
